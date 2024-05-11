2649e3f398d9e866fa962a48bfb96f8981d9109f
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-05-12 02:54:13 +0530

    Change

--- src/Urls.js
+++ src/Urls.js
@@ -1,2 +1,2 @@
-// export const REACT_BASE_URL ="http://localhost:4000/api/v1"
-export const REACT_BASE_URL ="https://task-management-system-server-baez.onrender.com"
+// export const REACT_BASE_URL ="http://localhost:8080/api/v1"
+export const REACT_BASE_URL ="https://task-management-system-server-kbtp.onrender.com"
