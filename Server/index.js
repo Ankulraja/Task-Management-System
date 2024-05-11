632fa3b979cde514eda30d60cdb70749e2b12709
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-05-11 14:36:21 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,11 +8,13 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://task-management-system-nine-khaki.vercel.app/dashboard/add-task",
     credentials: true,
   })
 );
-
+app.use("/",(req,res)=>{
+  res.send("Hello");
+})
 const PORT = process.env.PORT || 4000;
 dbConnect();
 app.use("/api/v1", taskRouter);
